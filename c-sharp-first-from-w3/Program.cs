67ff2d51179f5b9d11aea099092f6acfe66919ba
Shi Sha <emailtotheowner@gmail.com>
2020-10-12 19:26:31 -0400

    initial push

---
+++ c-sharp-first-from-w3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c_sharp_first_from_w3c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+        }
+    }
+}
