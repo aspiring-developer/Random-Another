c44177e5ad367e732d3ce68b3122c75a666eaf0c
Shi Sha <emailtotheowner@gmail.com>
2020-10-15 09:58:21 -0400

    Master page created

--- c-sharp-first-from-w3/Program.cs
+++ c-sharp-first-from-w3/Program.cs
@@ -2,14 +2,41 @@
 
 namespace c_sharp_first_from_w3c
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.Write("Hello World!");
-            Console.Write("Hello World!");
-        }
-    }
+class Animal  // Base class (parent) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+class Pig : Animal  // Derived class (child) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
+
+class Dog : Animal  // Derived class (child) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myPig = new Pig();  // Create a Pig object
+    Animal myDog = new Dog();  // Create a Dog object
+
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+}
 }
