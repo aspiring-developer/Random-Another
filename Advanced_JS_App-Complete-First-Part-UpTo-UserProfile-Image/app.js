d55bba5d852bb9a391f6365e1e4756da5c060feb
ssh1sharma <emailtotheowner@gmail.com>
2021-02-10 07:15:40 -0500

    Final version

--- Advanced_JS_App-Complete-First-Part-UpTo-UserProfile-Image/app.js
+++ Advanced_JS_App-Complete-First-Part-UpTo-UserProfile-Image/app.js
@@ -1,14 +1,14 @@
 let express = require("express");
 let session = require("express-session");
 let app = express();
-const MongoStore = require("connect-mongo") (session);
+const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
-let sessionOptions = session({secret: "I can do it!", store: new MongoStore({client: require('./db')}), resave: false, saveUninitialized: false, cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}});
+let sessionOptions = session({ secret: "I can do it!", store: new MongoStore({ client: require('./db') }), resave: false, saveUninitialized: false, cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true } });
 
 const router = require("./router");
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(sessionOptions);
 app.use(flash());
--- Advanced_JS_App-Complete-First-Part-UpTo-UserProfile-Image/db.js
+++ Advanced_JS_App-Complete-First-Part-UpTo-UserProfile-Image/db.js
@@ -2,12 +2,11 @@ const mongodb = require("mongodb");
 require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URI;
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
+mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   module.exports = client;
   const app = require('./app');
   let PORT = process.env.PORT || 3030;
-   app.listen(PORT, function(req, res) {
+  app.listen(PORT, function (req, res) {
     console.log(` App is listening on http://localhost:${PORT} `);
-    //console.log("MongoDB connected!");
   })
 })
