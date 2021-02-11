8602065bcb06cb0a24e587b57eadad44880c9586
ssh1sharma <emailtotheowner@gmail.com>
2021-02-11 18:15:53 -0500

    Database connection established

--- 1-MERN-FULLSTACK-SGNUP-LGN/server.js
+++ 1-MERN-FULLSTACK-SGNUP-LGN/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 let PORT = process.env.PORT || 3000;
+const connectDB = require('./config/db');
+
 let app = express();
+
+// Call the connectDB to connect to the database
+connectDB();
 
 app.get('/', function(req, res) {
   res.send("Welcome to my app!");
