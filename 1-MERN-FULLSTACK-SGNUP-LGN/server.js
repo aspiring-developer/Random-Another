e8281757832d53b2e4cfa14789c3a4f75abb08ae
ssh1sharma <emailtotheowner@gmail.com>
2021-02-13 12:30:53 -0500

    Body parser added

--- 1-MERN-FULLSTACK-SGNUP-LGN/server.js
+++ 1-MERN-FULLSTACK-SGNUP-LGN/server.js
@@ -8,6 +8,8 @@ let app = express();
 // Call the connectDB to connect to the database
 connectDB();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use('/api/users', usersRoute);
 
 app.get('/', function(req, res) {
