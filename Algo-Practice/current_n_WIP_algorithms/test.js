a491ef484defca6cf8081567b1fa72fd5276f905
ssh1sharma <emailtotheowner@gmail.com>
2021-03-03 17:38:14 -0500

    Project testing environment

--- Algo-Practice/current_n_WIP_algorithms/test.js
+++ Algo-Practice/current_n_WIP_algorithms/test.js
@@ -1,1 +1,16 @@
-console.log("test.js file:")
+//! Find max number
+function maxFinder(receivingNumbers) {
+  let maxNumber = -1000000;
+  for (i = 0; i < receivingNumbers.length; i++) {
+    if(receivingNumbers[i] > maxNumber){
+    maxNumber = receivingNumbers[i];
+    }
+  }
+  console.log(maxNumber);
+  return maxNumber;
+}
+
+let givenNumbers = [-100, 1, 5, 3, 7, 40, 9, 10, 265, 11]
+
+let foundMaxNumber = maxFinder(givenNumbers);
+console.log("Found Max Number: --> " + foundMaxNumber);
