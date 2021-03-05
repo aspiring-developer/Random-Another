03b222ae8d7f48bbefc2c5fdda7b3b8cff648be4
ssh1sharma <emailtotheowner@gmail.com>
2021-03-05 15:33:08 -0500

    New algo for 03/05/2021

---
+++ Algo-Practice/current_n_WIP_algorithms/finding_next_integer.js
@@ -0,0 +1,3 @@
+/* //! Write a function to find the smallest positive integer (greater than 0) in an array
+! *that does not occur in the array *constraints: 1 to 100,000 integers in the array; each element's value range is  -1,000,000 to 1,000,000  ###[1,3,6,4,1,2] should return 5 ###[1,2,3] should return 4 ###[-1,-3] should return 1*/
+
