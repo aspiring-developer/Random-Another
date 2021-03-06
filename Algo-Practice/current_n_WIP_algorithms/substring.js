ebd0e228d13dee79e7a3bacbe9caa6286585d956
ssh1sharma <emailtotheowner@gmail.com>
2021-03-06 15:21:54 -0500

    "commit test from VSC"

--- Algo-Practice/current_n_WIP_algorithms/substring.js
+++ Algo-Practice/current_n_WIP_algorithms/substring.js
@@ -1,5 +1,6 @@
 //TODO: Check if a substring is inside a string
 
+// Using built-in method
 function findSubStr(str, subStr) {
   if(str.includes(subStr)) {
     return true;
@@ -8,7 +9,12 @@ function findSubStr(str, subStr) {
   }
 }
 let result = findSubStr("hello", "llo");
-console.log(result);
+console.log(result + " <-- includes method ");
 
+// No built-in method
+function findSubStr1(str1, subStr1) {
+
+
+  }
 
 
