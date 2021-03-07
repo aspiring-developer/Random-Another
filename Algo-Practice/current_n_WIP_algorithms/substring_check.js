dd875c7fb9ac7a8a5af1b0d26342951213115f57
ssh1sharma <emailtotheowner@gmail.com>
2021-03-07 10:21:59 -0500

    new 03072021 learning

---
+++ Algo-Practice/current_n_WIP_algorithms/substring_check.js
@@ -0,0 +1,50 @@
+//TODO: Check if a substring is inside a string
+// Using built-in method
+//function findSubStr(str, subStr) {
+//  if(str.includes(subStr)) {
+//    return true;
+//  } else {
+//    return false;
+//  }
+//}
+//let result = findSubStr("hello", "llo");
+//console.log(result + " <-- Built-in method ");
+//abcde
+//ade
+
+//TODO: Check if a substring is inside a string --> Do not use built-in method
+function findSubStr1(str1, subStr1) {
+
+  let newStr = "";
+  var result;
+  var count = 0;
+
+  for (i = 0; i < str1.length; i++) {
+
+    var oneChar = str1[i]; //a
+    if (subStr1.startsWith(oneChar)) {
+      if (compareStrings(subStr1, (str1.substring(str1.indexOf(oneChar), subStr1.length)))) {
+        return true;
+
+      }else{
+        result = false;
+      }
+    }
+
+  }
+  return result;
+}
+
+console.log("Find Substr:"+findSubStr1("abcde", "cde"))
+
+
+function compareStrings(str3, str4) {
+  var result = true;
+  for (let i = 0; i < str3.length; i++) {
+    if (str3.charAt(i) !== str4.charAt(i)) {
+      result = false;
+    }
+
+  }
+  return result;
+}
