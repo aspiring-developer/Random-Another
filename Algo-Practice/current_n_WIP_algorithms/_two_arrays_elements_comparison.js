250461a972d975390f9edc577b4d51987acee400
ssh1sharma <emailtotheowner@gmail.com>
2021-03-08 12:22:39 -0500

    Comparison Algo

--- Algo-Practice/current_n_WIP_algorithms/_two_arrays_elements_comparison.js
+++ Algo-Practice/current_n_WIP_algorithms/_two_arrays_elements_comparison.js
@@ -3,6 +3,24 @@
 const arrayOne = [1, 4, 5, 7, 3, 8, 1, 9];
 const arrayTwo = [3, 7, 1, 12, 9, 5, 24, 16];
 
+function compareArrays(firstArr, secondArr) {
+notMatched = [];
+matched = [];
+for(i = 0; i < arrayOne.length; i++) {
+if(firstArr[i] === secondArr[i]) {
+  matched.push(secondArr[i]);
+  console.log(matched);
+  return matched;
+} else {
+  notMatched.push(firstArr[i])
+  return notMatched;
+
+}
+//console.log(notMatched);
+}
+
+}
+console.log("Final: --> " + compareArrays(arrayOne, arrayTwo));
 // arrayOne[i] === arrayOne[i]
 //1 -> 3, 7, 1, 12, 9, 5, 24, 16
 //4 -> 3, 7, 1, 12, 9, 5, 24, 16
