083481d6910c5918923376de8a8d1d105504ae1b
ssh1sharma <emailtotheowner@gmail.com>
2021-03-09 07:27:46 -0500

    Projects for 03092021

--- Algo-Practice/current_n_WIP_algorithms/vowel_reversed_return.js
+++ Algo-Practice/current_n_WIP_algorithms/vowel_reversed_return.js
@@ -1,15 +1,15 @@
 //TODO: Write a function that takes string as an input and returns the vowels reversed.
 
 function vowelReversed(str) {
-let foundVowels = [];
-let vowels = ["a", "e", "i", "o", "u"] ;
-for(i = 0; i < str.length; i++) {
-  if(str[i] === vowels[i]) {
-    foundVowels.push(str[i]);
+  let foundVowels = [];
+  let vowels = ['a', 'o', 'i', 'e', 'u'];
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === vowels[i]) {
+      foundVowels.push(str[i]);
+      console.log('foundVowels: --> ' + foundVowels);
+    }
   }
+console.log('foundVowels: --> ' + foundVowels);
+  return foundVowels;
 }
-console.log(foundVowels);
-//return foundVowels;
-}
-vowelReversed("hello")
-// hello
+console.log("Result: --> " +  vowelReversed("hello"));
