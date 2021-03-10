ea9cfd50479d3fbb14c4fe17569d18b111400b3b
ssh1sharma <emailtotheowner@gmail.com>
2021-03-10 11:13:18 -0500

    Algorithm of 03102021

--- Algo-Practice/current_n_WIP_algorithms/test.js
+++ Algo-Practice/current_n_WIP_algorithms/test.js
@@ -1,86 +1,28 @@
-//TODO: Given an array of integers, where all elements but one occur twice, find the unique element.
-
-//For example:
-//Given Array: givenArray = [1,2,3,4,3,2,1]
-//The unique element is 4.
-
-function lonelyIntFinder(arr) {
-  console.log(arr + " <-- Before sort");
-  let sortedArr = arr.sort();
-  console.log(arr + " <-- After sort");
-  let repeatedInt = [];
-  let lonelyInt = [];
-
-  for (let i = 0; i < sortedArr.length; i++) {
-  console.log("Array Element:" + sortedArr[i] + " | Index:  --> " + i);
-    let currentNum = sortedArr[i];
-    console.log("Current Num: " + currentNum);
-    let nextNum = currentNum + 1;
-    console.log("Next Num: " + nextNum);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//* Test recursion
+function recursiveFunction(num) {
+let recursiveSum = 0;
+for(i = 0; i <= num; i++) {
+if(recursiveSum <= num) {
+  //recursiveSum += recursiveFunction(num )
 
 
-    if (currentNum  === nextNum) {
-      repeatedInt.push(sortedArr[i]);
-      console.log("Repeated Integer: " + repeatedInt);
-    //console.log("Next Num in if: " + nextNum);
-    lonelyInt = currentNum;
-    console.log("Lonely Int in if: " + lonelyInt);
-    //return lonelyInt;
-
-    }
-    return lonelyInt;
-    }
- }
-let givenArray = [2, 3, 4, 3, 2,4,5];
-//let givenArray = [2, 1, 2];
-
-let finalResult = lonelyIntFinder(givenArray);
-console.log("Final Result:" + finalResult);
+//console.log(recursiveSum);
+//recursiveSum += num[i] ;
 
 
+console.log(recursiveSum);
+}
+}
+//console.log(recursiveFunction(num));
+return recursiveSum;
+}
 
+let givenNum = 10;
+console.log(recursiveFunction(givenNum) + " <-- Result")
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-
-
-
-
-
-
-
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//const findDuplicates = (arr) => {
-//  let sorted_arr = arr.slice().sort();
-//  let results = [];
-//  for (let i = 0; i < sorted_arr.length - 1; i++) {
-//    if (sorted_arr[i + 1] == sorted_arr[i]) {
-//      results.push(sorted_arr[i]);
-//    }
-//  }
-//  return results;
-//}
-
-//let duplicatedArray = [9, 9, 111, 2, 3, 4, 4, 5, 7];
-//console.log(`The duplicates in ${duplicatedArray} are ${findDuplicates(duplicatedArray)}`);
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>....
-//function eliminateDuplicates(arr) {
-//  var i,
-//      len = arr.length,
-//      out = [],
-//      obj = {};
-
-//  for (i = 0; i < arr.length; i++) {
-//    obj[arr[i]] = 0;
-//  }
-//  for (i in obj) {
-//    out.push(i);
-//  }
-//  return out;
-//}
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-
-
+/*
+num 10 -->
+1+2+3+4+5+6+7+8+9+10 = 55
+*/
