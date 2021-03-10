0bd1af405c33d94210c09e7e6baf33aa01bafab9
ssh1sharma <emailtotheowner@gmail.com>
2021-03-10 09:50:15 -0500

    Algorithm of 03102021

--- Algo-Practice/current_n_WIP_algorithms/_lonely_integer.js
+++ Algo-Practice/current_n_WIP_algorithms/_lonely_integer.js
@@ -3,16 +3,12 @@
 //For example:
 //Given Array: givenArray = [4, 7, 4, 3, 3, 16, 9, 7, 9]
 //The unique element is 16.
-
+//* One way to solve this:
 function lonelyIntFinder(arr) {
-  let lonelyInt;
-  console.log(arr + " <-- Before sort");
-  let sortedArr = arr.sort(function(a, b) {
-     return a - b;
-  });
-  console.log(sortedArr + " <-- After sort");
-  let i = 0;
+  let sortedArr = arr.sort(function(a, b) {return a - b});
+  console.log(sortedArr + " <-- Sorted array for While Loop");
 
+  let i = 0;
   while (i < sortedArr.length) {
     if (sortedArr[i] != sortedArr[i + 1]) {
       return sortedArr[i];
@@ -24,7 +20,29 @@ function lonelyIntFinder(arr) {
 let givenArray = [4, 7, 4, 3, 3, 16, 9, 7, 9];
 
 let finalResult = lonelyIntFinder(givenArray);
-console.log("Final Result:" + finalResult);
+console.log(finalResult + " <-- While Loop" );
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//* Another way of solving the algorithm
+function lonelyIntegerFinder(receivingArray) {
+  let pairedInteger = [];
+  let lonelyInteger = 0;
+  receivingArray.sort(function (a, b) { return a - b });
+  console.log(receivingArray + " <-- Sorted array for For Loop");
+
+  for (i = 0; i < receivingArray.length; i++) {
+    if (receivingArray[i] === receivingArray[i + 1] || receivingArray[i] === receivingArray[i - 1]) {
+      pairedInteger.push(receivingArray[i]);
+    }
+    else {
+      lonelyInteger += receivingArray[i];
+    }
+  }
+  return lonelyInteger;
+}
+let givenArray2 = [2, 4, 5, 1, 2, 3, 1, 5, 3,4];
+console.log(lonelyIntegerFinder(givenArray2) + " <-- For Loop");
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
