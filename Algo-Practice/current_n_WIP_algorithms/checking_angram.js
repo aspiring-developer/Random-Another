61dabca77c4e1d4c2829706a42ad373d8d37750b
ssh1sharma <emailtotheowner@gmail.com>
2021-03-11 12:39:44 -0500

    project 03112021

--- Algo-Practice/current_n_WIP_algorithms/checking_angram.js
+++ Algo-Practice/current_n_WIP_algorithms/checking_angram.js
@@ -2,68 +2,31 @@ function anagramCheck(str1, str2) {
   let strArray1 = [];
   let strArray2 = [];
 
-  // Convert str1 to array, sort and push
-  let sorted1 = str1.split('');
-  sorted1.sort();
-  strArray1.push(sorted1);
-  console.log(strArray1 + " <-- after str1 sort");
+  //THis is sujans
+  for (let i = 0; i < str1.length; i++) {
+    strArray1.push(str1[i]);
+    strArray2.push(str2[i]);
+  }
 
-  // Convert str1 to array, sort and push
-  let sorted2 = str2.split('');
-  sorted2.sort();
-  strArray2.push(sorted2);
-  console.log(strArray2 + " <-- after str2 sort");
+  console.log(strArray1);
+  console.log(strArray2);
+
+  strArray1.sort();
+  strArray2.sort();
+  console.log(strArray1.length);
+
+  console.log(strArray1);
+  console.log(strArray2);
+
 
   for (let i = 0; i < strArray1.length; i++) {
-    console.log(strArray1); //* This shows array
-    //console.log(strArray1.length); //* This is not working; shows 1
-    if (strArray1[i].indexOf(strArray2[i] === - 1)) {
-      //if (strArray1[i] !== strArray2[i]) {
-      console.log(strArray1[i].indexOf(strArray2[i]) + " <-- checking index");
-      return "False";
+    if (strArray1[i] != strArray2[i]) {
+      return false;
     }
-    return "True";
+
+    return true;
+
+
   }
 }
-console.log(anagramCheck("hello", "ohlle"));
-
-/* "hello"  "ohlle"   "ohgge" */
-// keep // peka // eekp
-
-//Declare 2 arrays
-//use for loop to insert into arrays
-//Sort both arrays
-//How to convert string to char array
-//How to sort an array
-
-//let str1Arr= ['k','e', 'e', 'p'];
-//let str2Arr= ['p','e', 'k', 'a'];
-
-//let sortedArr1= ['e','e', 'k', 'p'];
-//let sortedArr2= ['a','e', 'k', 'p'];
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//! IGNORE CODES BELOW
-//* Partially Working
-//function anagramFinder(str1, str2) {
-//  var i, j;
-//  let strArray1 = [];
-//  let strArray2 = [];
-//  for (i = 0, j = 0; i < str1.length, j < str2.length; i++, j++) {
-//    strArray1.push(str1[i]);
-//    strArray2.push(str2[j]);
-//  }
-//  console.log("String 1 array: --> " + [strArray1]);
-//  console.log("String 2 array: --> " + [strArray2]);
-//  return { strArray1, strArray2 };
-
-//}
-//console.log(anagramFinder("Hello", "Namaste"));
-
-//* STUDY THIS:
-//for (var i = 0, j = 1, n = 2; i < 50, n < 50; i = i + 3, j = j + 3, n = n + 3){
-//  console.log("variable i: " + i);
-//  console.log("variable j: " + j);
-//  console.log("variable n: " + n);
-//}
-
+  console.log(anagramCheck("shiva", "hiavm"));
--- Algo-Practice/current_n_WIP_algorithms/test_new.js
+++ Algo-Practice/current_n_WIP_algorithms/test_new.js
@@ -1,14 +1,38 @@
-function countCharacter(str) {
-  let charCount = [];
-  for (i = 0; i < str.length; i++) {
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-     if (charCount[str[i]] > 0) {
-      charCount[str[i]]++;
-    } else {
-      charCount[str[i]] = 1;
+function anagramCheck(str1, str2) {
+  let strArray1 = [];
+  let strArray2 = [];
+
+  for (let i = 0; i < str1.length; i++) {
+    strArray1.push(str1[i]);
+    strArray2.push(str2[i]);
+  }
+
+  console.log(strArray1);
+  console.log(strArray2);
+
+  strArray1.sort();
+  strArray2.sort();
+
+  console.log(strArray1);
+  console.log(strArray2);
+
+
+
+  let result = '';
+
+  for (let i = 0; i < strArray1.length; i++) {
+    console.log(strArray1[i] + " <-- from 1");
+    console.log(strArray2[i] + " <-- from 2");
+    //if (JSON.stringify(strArray1[i]) !== JSON.stringify(strArray2[i])) {
+    if (strArray1 !== strArray2[i]) {
+result = "Not equal";
+      //return false;
     }
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    result = "Equal";
+    //return true;
+
+
+  }
+  return result;
 }
-return charCount;
-}
-console.log(countCharacter('javascript'));
+  console.log(anagramCheck("shiva", "hiavm"));
