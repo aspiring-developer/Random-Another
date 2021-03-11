5a002235daf0bfca8bfd95b284f266ba37ef92af
ssh1sharma <emailtotheowner@gmail.com>
2021-03-10 19:28:52 -0500

    Algorithm of 03102021

--- Algo-Practice/current_n_WIP_algorithms/checking_angram.js
+++ Algo-Practice/current_n_WIP_algorithms/checking_angram.js
@@ -1,15 +1,41 @@
 // Find if the given string is anagram
+// keep  // peke  // eekp
 
-// keep
-// peka
-// eekp
-
-function isAnagram(str1, str2) {
+function isAnagram(arr) {
+  let convertedArray1 = [];
+  let convertedArray2 = [];
 
 
-  return true;
+  //* First Loop to sort array of first string
+  for (i = 0; i < arr[0].length; i++) {
+    convertedArray1.push(arr[0][i]);
+  }
+  let sorted1 = convertedArray1.sort();
+  console.log(sorted1 + " <-- sorted1");
+
+  //* Second Loop to sort array of second string
+  for (j = 0; j < arr[1].length; j++) {
+    convertedArray2.push(arr[1][j]);
+  }
+  let sorted2 = convertedArray2.sort();
+  console.log(sorted2 + " <-- sorted2");
+
+let newArr = [sorted1, sorted2];
+
+  //* Now, comparing both sorted arrays
+  if (newArr[0] === newArr[1]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+let givenArray = ["keep", "keep"];
+console.log(isAnagram(givenArray) + " <-- Result");
+
+
+
+//TODO: For reference purpose
 //Declare 2 arrays
 //use for loop to insert into arrays
 //Sort both arrays
@@ -18,13 +44,13 @@ function isAnagram(str1, str2) {
 //How to sort an array
 
 
-let str1Arr= ['k','e', 'e', 'p'];
-let str2Arr= ['p','e', 'k', 'a'];
+//let str1Arr= ['k','e', 'e', 'p'];
+//let str2Arr= ['p','e', 'k', 'a'];
 
-let sortedArr1= ['e','e', 'k', 'p'];
-let sortedArr2= ['a','e', 'k', 'p'];
+//let sortedArr1= ['e','e', 'k', 'p'];
+//let sortedArr2= ['a','e', 'k', 'p'];
 
-//use for loop to compare
-if(sortedArr1[i] != sortedArr2[i]){
+////use for loop to compare
+//if(sortedArr1[i] != sortedArr2[i]){
 
-}
+//}
--- Algo-Practice/current_n_WIP_algorithms/test.js
+++ Algo-Practice/current_n_WIP_algorithms/test.js
@@ -1,20 +1,31 @@
-function isAnagram(str1, str2) {
-  let str1ToArray = [];
-  let str2ToArray = [];
+//* This test loop w/o function is working
+//var i, j;
+//let str1 = "Hello";
+//let str2 = "Namaste";
+//let strArray1 = [];
+//let strArray2 = [];
+//for (i = 0, j = 0; i < str1.length, j < str2.length; i++, j++) {
+//  strArray1.push(str1[i]);
+//  strArray2.push(str2[i]);
+//}
+//console.log("String 1 array: --> " + [strArray1]);
+//console.log("String 2 array: --> " + [strArray2]);
 
-  //for (let i = 0, j = 0; i < str1.length, j < str2.length; i++, j++){
-  for (let i = 0; i < str2.length; i++){
-    //console.log("variable i: " + i);
-    //console.log("variable j: " + j);
-
-  //str1ToArray.push(str1[i]);
-str2ToArray.push(str2[i]);
+//* Function trial
+function anagramFinder(str1, str2) {
+  var i, j;
+  let strArray1 = [];
+  let strArray2 = [];
+  for (i = 0, j = 0; i < str1.length, j < str2.length; i++, j++) {
+    strArray1.push(str1[i]);
+    strArray2.push(str2[j]);
   }
-  return str2ToArray;
-}
-  let givenArray = ["hello", "namaste"];
-  console.log(isAnagram(givenArray));
+  console.log("String 1 array: --> " + [strArray1]);
+  console.log("String 2 array: --> " + [strArray2]);
+  return { strArray1, strArray2 };
 
+}
+console.log(anagramFinder("Hello", "Namaste"));
 
 //* STUDY THIS:
 //for (var i = 0, j = 1, n = 2; i < 50, n < 50; i = i + 3, j = j + 3, n = n + 3){
@@ -22,3 +33,4 @@ str2ToArray.push(str2[i]);
 //  console.log("variable j: " + j);
 //  console.log("variable n: " + n);
 //}
+
