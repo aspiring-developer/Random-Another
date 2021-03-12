272832e6d1b1e8f231d93938a2fe423eac104cba
ssh1sharma <emailtotheowner@gmail.com>
2021-03-12 09:08:57 -0500

    Project set up for 03122021

--- Algo-Practice/current_n_WIP_algorithms/test_new.js
+++ Algo-Practice/current_n_WIP_algorithms/test_new.js
@@ -1,31 +1,70 @@
-//TODO: Compare two arrays and find unique values (element that only exists in one array):
+//TODO: Find Missing Number
 
-const arrayOne = [2, 1, 4, 5, 7, 3, 8, 1, 9, 10, 12, 13, 15];
-const arrayTwo = [3, 7, 1, 12, 9, 5, 24, 16];
-
-//* Comparing first array to second
-function compareFirstToSecond(firstArr, secondArr) {
-  result1 = [];
-  for (i = 0; i < firstArr.length; i++) {
-    if (secondArr.indexOf(firstArr[i]) === -1) {
-      result1.push(firstArr[i]);
+/*
+- Write a function
+- Find the max and min number in the given array
+- Find the sum of the given array elements
+- Find the sum of the consecutive numbers 1 to the max number
+- Find the missing number by subtracting the given numbers sum from the consecutive numbers sum
+- Return result
+*/
+function missingNumberFinder(num) {
+  //* Find Max Num
+  let maxNum = -1000000;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] > maxNum) {
+      maxNum = num[i];
     }
   }
-  return result1;
-}
-console.log("Final: --> " + compareFirstToSecond(arrayOne, arrayTwo));
+  //return maxNum;
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//* Comparing second array to first
-function compareSecondToFirst(firstArr, secondArr) {
-  result2 = [];
-  for (i = 0; i < secondArr.length; i++) {
-    if (firstArr.indexOf(secondArr[i]) === -1) {
-      result2.push(secondArr[i]);
+  //* Find Min Num
+  let minNum = 1000000;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] < minNum) {
+      minNum = num[i];
     }
   }
-  return result2;
+
+  //*Finding sum of consecutive
+  let sumOfConsecutive = 0;
+  for (let i = minNum; i <= maxNum; i++) {
+    sumOfConsecutive += i;
+  }
+  console.log(sumOfConsecutive)
+  //return sumOfConsecutive;
+
+
+  // Calling sumOfGivenNumbers from sum of given numbers
+  let sumOfGiven = sumOfGivenNumbers(givenNumber);
+
+  // Calling sumOfConsecutiveNumbers from sum of consecutive numbers
+  //let sumOfConsecutive = sumOfConsecutiveNumbers(givenNumber);
+
+  let toViewInConsole = `Max Num: ${maxNum}, Min Num: ${minNum} \n Sum of Given Numbers: ${sumOfGiven}  \n Sum of Consecutive Numbers: ${sumOfConsecutive} `;
+  return toViewInConsole;
 }
-console.log("Final: --> " + compareSecondToFirst(arrayOne, arrayTwo));
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+const givenNumber = [4, 7, 8, 3, 6];
+console.log(`Result: \n ${missingNumberFinder(givenNumber)} `)
+
+
+//* Find sum of given numbers
+function sumOfGivenNumbers(num) {
+  let sumOfGiven = 0;
+  for (let i = 0; i < num.length; i++) {
+    sumOfGiven += num[i];
+  }
+  return sumOfGiven;
+}
+
+//* Find sum of consecutive numbers in given array
+//function sumOfConsecutiveNumbers(num) {
+//  let sumOfConsecutive = 0;
+//  for (let i = minNum; i < num.length; i++) {
+//    sumOfConsecutive += num[i];
+//  }
+//  console.log(sumOfConsecutive)
+//  //return sumOfConsecutive;
+//}
+
 
