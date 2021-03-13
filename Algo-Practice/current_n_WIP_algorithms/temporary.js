191003b5518984ae48603a827a595113e785dc3c
ssh1sharma <emailtotheowner@gmail.com>
2021-03-13 10:52:25 -0500

    Project for 03132021

--- Algo-Practice/current_n_WIP_algorithms/temporary.js
+++ Algo-Practice/current_n_WIP_algorithms/temporary.js
@@ -13,7 +13,10 @@ console.table("* " + fruits + " <--Adds 'Guava' and 'Cherry' ") // prints items 
 fruits.unshift("Papaya", "Raspberry") // adds new items at the beginning
 console.log("* " + "Adds 'Papaya'and 'Raspberry' --> " + fruits) // prints items and their index in a table
 
-fruits.pop("Guava") // removes an item from the end
+fruits.pop() // removes an item from the end
 console.table("* " + fruits + " <--Removes 'Cherry' from the end ") // prints items and their index in a table
-fruits.unshift("Blueberry") // adds a new item at the beginning
-console.log("* " + "Adds 'Blueberry' at the beginning--> " + fruits) // prints items and their index in a table
+fruits.shift() // removes an item from the beginning
+console.log("* " + "Removes 'Papaya' from the beginning--> " + fruits) // prints items and their index in a table
+
+console.log("* " + typeof fruits + " <--Data type of fruits variable "); // returns the data type; arrays data type is 'object'
+console.log("* " + Array.isArray(fruits) + " <--Checks if it is an array "); // confirms if the array is an array or not
