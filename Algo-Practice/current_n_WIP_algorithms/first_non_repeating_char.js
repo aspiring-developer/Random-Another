417051b0ede27654834d5419297abb04669b3eb5
ssh1sharma <emailtotheowner@gmail.com>
2021-03-13 16:11:55 -0500

    Non repeating char complete

--- Algo-Practice/current_n_WIP_algorithms/first_non_repeating_char.js
+++ Algo-Practice/current_n_WIP_algorithms/first_non_repeating_char.js
@@ -1,5 +1,28 @@
+//TODO: Find first non-repeating character in a string
 
-//Find first non-repeating character in a string
+function firstLonelyChar(str) {
+  let foundCharacter;
+
+  for (let i = 0; i < str.length; i++) {
+    //console.log(str.charAt(i) + " <--CharAt value");
+    foundCharacter = str.charAt(i); //b
+
+    if (str.indexOf(foundCharacter) === i && str.indexOf(foundCharacter, i + 1) === -1) {
+
+      // indexOF => 1 === i = 1   && indexOf = -1 === -1
+
+      console.log("...............\n" + foundCharacter + " <--Found Character" + "\n...............");
+      return foundCharacter;
+    }
+    //else {
+    //  return null;
+    //}
+  }
+  return null;
+}
+
+let givenString = "bbaarrghh";
+console.log(` ${firstLonelyChar(givenString)} <--Final Result `);
 
 
 
@@ -11,9 +34,7 @@
 
 
 
-
-
-
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //apple -> a
 //str.indexOf('a', 1)
 //var one = 0;
---
+++ Algo-Practice/current_n_WIP_algorithms/quick_test_only.js
@@ -0,0 +1,10 @@
+class Car {
+  //constructor(name, year) {
+  //  //this.name;
+  //  //this.year;
+  //}
+}
+let myCar = new Car();
+myCar.name = "Toyota";
+myCar.year = 1997;
+console.log(myCar.name, myCar.year);
