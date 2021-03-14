257dcb2715c2e11528ee05a61f6b81943554f2a8
ssh1sharma <emailtotheowner@gmail.com>
2021-03-14 15:00:53 -0400

    New algo refresher

--- Algo-Practice/current_n_WIP_algorithms/test.js
+++ Algo-Practice/current_n_WIP_algorithms/test.js
@@ -1,15 +1,69 @@
-//TODO: Find Max and Min num in an array
+//TODO: Find Missing Number in an array
+function missingNumFinder(numArray) {
+// find min and max in the array
 
-function maxMinFinder(num) {
-let maxNum = -1000000;
-let minNum = 1000000;
-for (let i = 0; i < num.length; i++) {
-if(num[i] > maxNum) {
-  maxNum = num[i];
+let maxInArray = Math.max.apply(Math, numArray);
+let minInArray = Math.min.apply(Math, numArray);
+console.log(`Max: ${maxInArray} | Min: ${minInArray}`)
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// find sum of the array numbers
+function sumOfGivenFinder(numArray) {
+  let sumOfGivenNumbers = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    sumOfGivenNumbers += numArray[i];
+  }
+  console.log(sumOfGivenNumbers + " <--Sum of given")
+  return sumOfGivenNumbers;
+
+  }
+console.log(sumOfGivenFinder(givenNumForMissing));
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// find sum of the consecutive numbers that starts from min and ends to max
+function sumOfConsecutiveFinder(min, max) {
+  let sumOfConsecutiveNumbers = 0;
+  for (let i = min; i <= max; i++) {
+    sumOfConsecutiveNumbers += i;
+  }
+  console.log(sumOfConsecutiveNumbers + " <--Sum of consecutive")
+  return sumOfConsecutiveNumbers;
+  }
+  console.log(sumOfConsecutiveFinder(minInArray, maxInArray));
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+let missingNumber = 0;
+missingNumber = sumOfConsecutiveNumbers - sumOfGivenNumbers;
+
+
+  return missingNumber;
 }
-}
-return maxNum;
-//return "????"
-}
-let givenNum = [4, 7, 8, 3, 6];
-console.log(maxMinFinder(givenNum));
+
+let givenNumForMissing = [4, 7, 8, 3, 6,9];
+console.log(missingNumFinder(givenNumForMissing));
+
+
+
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//TODO: Find Max and Min num in an array
+//function maxMinFinder(num) {
+//let maxNum = -1000000;
+//let minNum = 1000000;
+//for (let i = 0; i < num.length; i++) {
+//if(num[i] > maxNum) {
+//  maxNum = num[i];
+//}
+//if(num[i] < minNum) {
+//  minNum = num[i];
+//}
+//}
+//return {maxNum, minNum};
+//}
+//let givenNum = [4, 7, 8, 3, 6];
+//console.log(maxMinFinder(givenNum));
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
