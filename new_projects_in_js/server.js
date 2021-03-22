1c154d30fedc83a50a112a9803a405c9017af9ff
ssh1sharma <emailtotheowner@gmail.com>
2021-03-22 10:47:34 -0400

    App and db connection established

--- new_projects_in_js/server.js
+++ new_projects_in_js/server.js
@@ -1,4 +1,7 @@
 let express = require('express');
+let mongodb = require('mongodb');
+require('dotenv').config();
+
 let app = express();
 let PORT = process.env.PORT || 3000;
 
@@ -7,12 +10,22 @@ app.use(express.json());
 
 app.get('/', function(req, res) {
   res.send("Welcome to my app!!!")
+  console.log("Working app.get");
 });
 app.post('/register', function(req, res) {
-  console.log("Working")
+ console.log(req.body);
+  console.log("Working app.post");
   res.send("Thank you for registering.")
 });
 
-app.listen(3000, function(req, res) {
-console.log(` App is running on http://localhost:${PORT}`)
-});
+let connectionString = process.env.MONGO_URI;
+mongodb.connect(connectionString, { useUnifiedTopology: true }, function(err, client) {
+  let db = client.db();
+  db.collection("coll-03222021").insertOne({ id: 1, firstName: 'Shi', lastName: 'Sha' })
+  console.log("Database connected successfully!")
+
+  app.listen(3000, function(req, res) {
+  console.log(` App is running on http://localhost:${PORT}`)
+  });
+})
+
