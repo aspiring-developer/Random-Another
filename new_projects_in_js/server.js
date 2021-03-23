691b6a06f5ae424c8bac3103dc4623287bdd2ba0
ssh1sharma <emailtotheowner@gmail.com>
2021-03-23 09:40:17 -0400

    New project 03232021

--- new_projects_in_js/server.js
+++ new_projects_in_js/server.js
@@ -1,31 +1,20 @@
-let express = require('express');
-let mongodb = require('mongodb');
-require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-let app = express();
-let PORT = process.env.PORT || 3000;
-
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.send("Welcome to my app!!!")
-  console.log("Working app.get");
-});
-app.post('/register', function(req, res) {
- console.log(req.body);
-  console.log("Working app.post");
-  res.send("Thank you for registering.")
+res.send(` <html><head></head><body><div style="text-align:center; background-color: #aeaeae"><form action="create-item" method="POST"><input type="text" name="userInput" /><button type="submit">Submit</button></form></div><div><ul><li><span class="itemName">Temporary Placeholder Item 1</span><button class="editButton">Edit</button><button class="deleteButton">Delete</button></li></ul></div></body></html> `);
 });
 
-let connectionString = process.env.MONGO_URI;
-mongodb.connect(connectionString, { useUnifiedTopology: true }, function(err, client) {
-  let db = client.db();
-  db.collection("coll-03222021").insertOne({ id: 1, firstName: 'Shi', lastName: 'Sha' })
-  console.log("Database connected successfully!")
-
-  app.listen(3000, function(req, res) {
-  console.log(` App is running on http://localhost:${PORT}`)
+app.post('/create-item', function(req, res) {
+  console.log(req.body.userInput);
+  res.send(` <html><head></head><body><div><ul><li><span class="itemName">${req.body.userInput} Temporary Placeholder Item 1</span><button class="editButton">Edit</button><button class="deleteButton">Delete</button></li></ul></div></body></html> `)
+  res.redirect('/');
   });
-})
 
+app.listen(PORT, function(req, res) {
+console.log(`App is running on http://localhost:${PORT}`)
+});
