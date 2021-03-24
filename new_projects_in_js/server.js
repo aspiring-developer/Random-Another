fe75fa2dc2658f407b7dfea8c159c34f84fa972e
ssh1sharma <emailtotheowner@gmail.com>
2021-03-24 15:12:38 -0400

    Testing env created

--- new_projects_in_js/server.js
+++ new_projects_in_js/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+let connectionString = process.env.MONGO_URI;
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', function(req, res) {
 res.send(` <html><head></head><body><div style="text-align:center; background-color: #aeaeae"><form action="create-item" method="POST"><input type="text" name="userInput" /><button type="submit">Submit</button></form></div><div><ul><li><span class="itemName">Temporary Placeholder Item 1</span><button class="editButton">Edit</button><button class="deleteButton">Delete</button></li></ul></div></body></html> `);
--- new_projects_in_js/test.js
+++ new_projects_in_js/test.js
@@ -1,12 +1,15 @@
-function outer(){
-  let counter = 0;
-  function incrementCounter() {
-    counter ++;
-  }
-return incrementCounter;
-}
-const myNewFunction = outer();
-//myNewFunction();
-//myNewFunction();
-console.log(myNewFunction() + " <--Calling myNewFunction()");
+//let result = []; // Initialize array!
 
+//result[0] = 0;
+//result[1] = 1;
+//for (let i = 2; i <= 6; i++) {
+//  // Next fibonacci number = previous + one before previous
+//   result[i] = result[i - 2] + result[i - 1];
+//  console.log(result[i]);
+//}
+
+var fib = [0, 1];
+for (var i = fib.length; i < 10; i++) {
+  fib[i] = fib[i - 2] + fib[i - 1];
+}
+console.log(fib);
