0ac0008c78ec3418996042b35e0be002554ea55f
ssh1sharma <emailtotheowner@gmail.com>
2021-03-26 09:38:05 -0400

    New project file/folder organized

--- new_projects_in_js/server.js
+++ new_projects_in_js/server.js
@@ -1,30 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 const ejs = require('ejs');
-require('dotenv').config();
-
 const app = express();
+
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+//app.set('views', './views');
+//app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-let connectionString = process.env.MONGO_URI;
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
-
-app.get('/', function(req, res) {
-res.send(` <html><head></head><body><div style="text-align:center; background-color: #aeaeae"><form action="create-item" method="POST"><input type="text" name="userInput" /><button type="submit">Submit</button></form></div><div><ul><li><span class="itemName">Temporary Placeholder Item 1</span><button class="editButton">Edit</button><button class="deleteButton">Delete</button></li></ul></div></body></html> `);
+app.get('/', function(req,res) {
+  res.render("./index")
 });
 
-app.post('/create-item', function(req, res) {
-  console.log(req.body.userInput);
-  res.send(` <html><head></head><body><div><ul><li><span class="itemName">${req.body.userInput} Temporary Placeholder Item 1</span><button class="editButton">Edit</button><button class="deleteButton">Delete</button></li></ul></div></body></html> `)
-  res.redirect('/');
-  });
+app.get('/graphic-digital', function(req,res) {
+  res.render("graphicDigital")
+});
+
 
 app.listen(PORT, function(req, res) {
-console.log(`App is running on http://localhost:${PORT}`)
-});
+  console.log(` App is running on http://localhost:3000 `);
+})
+
